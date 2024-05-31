dcd349187d394fce0df5b83d6bad756559bd498e
zacharyzimm <105132282+zacharyzimm@users.noreply.github.com>
2024-05-31 02:01:05 -0700

    add check in server/__init__ for successful prod env load

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -5,3 +5,7 @@ from dotenv import load_dotenv
 
 logging.info("Loading env...")
 load_dotenv()
+
+if os.environ.get("env") == "prod":
+    if not os.environ.get("SUCCESSFUL_DOTENV_LOAD") == "prod_env":
+        raise Exception("CRITICAL ERROR: environment variables not successfully loaded")
