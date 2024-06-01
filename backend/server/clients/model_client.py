ad4eb38c492ff7588ba7ecb1a4c953d5bc9b9105
zacharyzimm <105132282+zacharyzimm@users.noreply.github.com>
2024-05-31 22:43:42 -0700

    filling out more model client code

--- backend/server/clients/model_client.py
+++ backend/server/clients/model_client.py
@@ -1,15 +1,26 @@
+import os
+import requests
 
 
-class ModelClient():
+class ModelClient:
     """
     Client that talks to the model deployment and returns a response
     """
 
     def __init__(self):
+        self.google_maps_api_key = os.environ.get("GOOGLE_MAPS_API_KEY")
+        self.bing_maps_api_key = os.environ.get("BING_MAPS_API_KEY")
+
+    def talk_to_model(self):
         pass
 
     def process_image(self):
         image = self.talk_to_model()
+        return image
+
+    def annotate_image(self):
 
     def get_response(self):
         image = self.process_image()
+        annotated_image = self.annotate_image()
+        return annotated_image
